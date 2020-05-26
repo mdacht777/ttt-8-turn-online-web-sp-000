b9635b9740cc32d1fd67da6c0067212e81ec9462
Martin Achtermann <mdacht777@gmail.com>
2020-05-26 01:32:47 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,1 +1,8 @@
 #!/usr/bin/env ruby
+puts "Welcome to Tic Tac Toe!"
+board=[" "," "," "," "," "," "," "," "," "]
+puts "Where would you like to go?"
+input=gets.strip
+move(board,input_to_index(input),"X")
+display_board(board)
+
