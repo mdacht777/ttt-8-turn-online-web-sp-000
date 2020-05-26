3c5d71118b6093ea5cf318efe9a17d41f1aa6e8d
Martin Achtermann <mdacht777@gmail.com>
2020-05-26 01:33:01 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,3 +6,19 @@ input=gets.strip
 move(board,input_to_index(input),"X")
 display_board(board)
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+def input_to_index(input)
+  input.to_i-1
+end
+
+def move(board,index,char="X")
+  board[index]=char
+end
