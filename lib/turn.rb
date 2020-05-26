13b3d4fa2ea6b1e916927edd30ebffacf9680fb5
Martin Achtermann <mdacht777@gmail.com>
2020-05-26 01:44:30 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -22,3 +22,12 @@ def move(board,index,char="X")
   board[index]=char
 end
 
+# code your #valid_move? method here
+def valid_move?(board,index)
+  index.between?(0, 8) && !position_taken?(board,index)   
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board,index)
+  !board[index]==" " || board[index]=="X" || board[index]=="O"
+end
